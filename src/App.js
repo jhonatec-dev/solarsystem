c53f9dec04e83f61a68e7affd454315668925d08
jhonsreis <jhonsreis@gmail.com>
2023-04-23 21:30:14 -0300

    Ícones e disposição consertadas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import './App.css';
 import Header from './components/Header';
 import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
+import backSound from './images/backSound.mp3';
+
+const song = new Audio(backSound);
 
 // E Aqui começa a jornada para além de meu planeta natal...
 class App extends React.Component {
+  playSong = () => {
+    song.volume = 0.3;
+    song.loop = true;
+    song.play();
+  };
+
   render() {
     return (
-      <div className="App">
+      <div
+        className="App"
+        onClick={ this.playSong }
+        role="button"
+        aria-hidden
+      >
         <Header />
         <SolarSystem />
         <Missions />
